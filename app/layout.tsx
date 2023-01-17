50c15d0ff04771be5e796939fa15dea1c703b486
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-01-17 22:47:07 +0900

    style: Spoqa Han Sans 로컬 폰트 적용 (#6)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,40 @@
 import "styles/globals.scss";
 
 import AuthContext from "app/AuthContext";
-import { getServerSession } from "lib/next-auth";
-import React from "react";
+import { getServerSession } from "libs/auth";
+
+import localFont from "@next/font/local";
+
+// Next13 폰트 최적화
+const font = localFont({
+  src: [
+    {
+      path: "../assets/fonts/SpoqaHanSansNeo-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/SpoqaHanSansNeo-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/SpoqaHanSansNeo-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/SpoqaHanSansNeo-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/SpoqaHanSansNeo-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export default async function RootLayout({
   children,
@@ -13,13 +45,9 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <AuthContext session={session}>
-        <body>{children}</body>
+        <body className={font.className}>{children}</body>
       </AuthContext>
     </html>
   );

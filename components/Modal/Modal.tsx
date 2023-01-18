1076bc821f06753f0dbef555a5db77f27a4daf1a
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-01-18 23:20:31 +0900

    fix: onClose props 타입을 any에서 Function 으로 수정

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -4,9 +4,9 @@ import ModalBackground from "components/Modal/ModalBackground";
 import ModalContainer from "components/Modal/ModalContainer";
 import ModalPortal from "components/Modal/ModalPortal";
 import EventType from "types/event";
-import { ModalProps } from "types/modal";
+import { ModalComponentProps } from "types/modal";
 
-interface Props extends ModalProps {}
+interface Props extends ModalComponentProps {}
 
 export default function Modal({ children, onClose, ...props }: Props) {
   const backgroundClickHandler: EventType<"div", "onClick"> = (e) => {

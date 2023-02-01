669d441fc594a57b4ee4f2f47ef7be52f2b445f1
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-02-01 18:45:27 +0900

    feat: useInput 훅에서 에러 메세지 핸들링 기능 추가

--- hooks/useInput.ts
+++ hooks/useInput.ts
@@ -1,12 +1,44 @@
 import { useState } from "react";
 import EventType from "types/Event";
 
-export default function useInput(initialValue: string = "") {
+type InputErrorHandler = (error: string) => string;
+
+export interface UseInputProps {
+  initialValue?: string;
+  validate?: (value: string, errorHandler: InputErrorHandler) => string;
+  errorHandler?: InputErrorHandler;
+}
+
+export default function useInput({
+  initialValue = "",
+  validate = () => "",
+  errorHandler = () => "",
+}: UseInputProps) {
   const [input, setInput] = useState(initialValue);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const isError = 0 < errorMessage.length;
 
   const changeHandler: EventType<"input", "onChange"> = (e) => {
     setInput(e.target.value);
+    setErrorMessage(validate(input, errorHandler));
   };
 
-  return { input, changeHandler };
+  const handleInputError = (error: string) => {
+    setErrorMessage(errorHandler(error));
+  };
+
+  const resetValue = () => {
+    setInput("");
+    setErrorMessage("");
+  };
+
+  return {
+    input,
+    isError,
+    errorMessage,
+    changeHandler,
+    handleInputError,
+    resetValue,
+  };
 }

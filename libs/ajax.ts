4c405318423f5e83c136125f72fadaf0a213a562
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-02-01 18:46:01 +0900

    feat: axios timeout 시간 추가

--- libs/ajax.ts
+++ libs/ajax.ts
@@ -1,8 +1,11 @@
 import axios, { AxiosError } from "axios";
 import { API_SERVER_URL } from "configs/env.client";
 
+// 인증이 필요하지 않은 API 요청을 위한 axios 인스턴스
 export const apiRequest = axios.create({
   baseURL: API_SERVER_URL,
+  timeout: 2500 /* 2.5ms */,
 });
 
+// axios 요청으로 인한 에러인지 체크하는 함수
 export const isAxiosError = (e: any): e is AxiosError => axios.isAxiosError(e);
